8ea19d57c0a24a5dc680c7da39d117f8cd7c78a9
mouse <kishorkumarl9088@gmail.com>
2026-05-30 19:26:47 +0530

    Connected frontend to deployed backend

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://community-discussion-forum-realtime-chat.onrender.com/api",
 });
 
 export default API;
